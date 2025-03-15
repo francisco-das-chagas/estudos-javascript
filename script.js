c2816b7a87f232a59845faa0ac300e68614e8b25
unknown <f.chagasdutra18@gmail.com>
2025-03-15 19:26:18 -0300

    funcoes

--- script.js
+++ script.js
@@ -1,34 +1,65 @@
-// Um objeto é uma forma de mantermos dados associados e organizados. Ele é 
-// criado com chaves {}. "Tudo é objeto", vocês já vão ver o motivo.
 
-const nome = 'O Senhor dos Anéis';
-const ano = 1954;
-const autor = 'J. R. R. Tolkien';
+//           FUNÇÕES
+// As funções (Function) são blocos de código que podem ser executados. 
+// A execução pode acontecer em diferentes cenários e diversas vezes. 
+// Para executar utilizamos os parênteses ();
 
-const nomeFilme = 'O senhor dos Anéis';
-const anoFilme = 2001;
-const diretorFilme = 'Peter Jackson';
-
-const livro = {
-    nome: 'O Senhor dos Anéis',
-    ano: 1954,
-    autor: 'J. R. R. Tolkien',
-};
-const filme = {
-  nome: 'O Senhor dos Anéis',
-  ano: 2001,
-  autor: 'J. R. R. Tolkien',
-  diretor: 'Peter Jackson'
-
+function logCursos() {
+  const nome = 'Javascript';
+  const horas = '40 Horas';
+  console.log(nome, horas);
 }
-console.log(livro.nome === nome)
 
-const texto = document.querySelector("p");
+logCursos();
+logCursos();
+logCursos();
 
-console.log(texto.clientHeight)
+// Parâmetro e Argumento
+// Podemos definir parâmetros durante a criação da função. 
+// Durante a execução, podemos passar argumentos ('nome'), 
+// que serão utilizados nos parâmetros.
 
-console.log(autor.toLocaleLowerCase())
-const decimal = 122.99
+// nome e horas são os parâmetros
+function logCursos(nome, horas) {;  
+  console.log(nome, horas, 'horas');
+}
+// JavaScript e 40 são os argumentos
+logCursos('javascript', '40');
+logCursos('HTML', '20');
+logCursos('CSS', '30');
 
-console.log(Number(decimal.toFixed()) + 3)
-console.log(3)
+// Retorno
+// Toda função retorna um valor (tipo de dado). 
+// Se o retorno não for declarado utilizado a palavra return, 
+// o valor retornado será undefined.
+
+function logCursos(nome) {
+  console.log(nome);
+  return nome + ' Log'
+}
+
+const valor = logCursos('JavaScript');
+console.log(valor)
+
+// Escopo
+// Uma função cria o seu próprio escopo. 
+// Isso significa que variáveis declaradas dentro
+// de uma função, não podem ser acessadas fora das mesmas. 
+// Porém, variáveis criadas fora da função,
+// podem ser acessadas pela mesma.
+const nome = 'JavaScript'
+
+function logCursos() {
+  console.log(nome);
+}
+// Loga JavaScript
+logCursos();
+
+function logLivro() {
+  const livro = 'O Senhor dos Anéis';
+  console.log(livro)
+}
+logLivro();
+
+//Erro, pois a constante livro não existe
+console.log(livro);
