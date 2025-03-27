f087ec99054a4841ea1f83c7c8139f43fefd2ddb
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-03-27 00:23:21 -0300

    finalizando exercicio

--- aulas/java-script-aulas/solucionar-problemas/script.js
+++ aulas/java-script-aulas/solucionar-problemas/script.js
@@ -1,24 +1,13 @@
-// 1 - Adicione uma navegação (nav) com 3 links: ./, ./produto.html e ./contato.html
-// 2 - Crie as páginas produto.html e contato.html com o mesmo html de index.html
-// 3 - Selecione os links na navegação.
-// 4 - Para cada link execute uma função (ativarLink);
-// 5 - Na função, extraia o href do elemento.
-// 6 - Verifique se o href é igual ao URL da página (document.location.href)
-// 7 - Se for igual mude o backgroundColor para black e a color para white do link.
+const imagens = document.querySelectorAll("#galeria li img");
 
-const links = document.querySelectorAll("nav a");
-
-function ativarLink(link) {
-  const href = link.href;
-  const url = document.location.href;
-
-  if(href === url) {
-    link.style.backgroundColor = "black";
-    link.style.color = "white"
-  }
-
-  console.log(url);
-  console.log(href)
+function galeriaTrocar(event) {
+  const principal = document.querySelector("#imagem-principal");
+  const clicado = event.currentTarget;
+  principal.src = clicado.src;
+  principal.alt = clicado.alt;
 }
 
-links.forEach(ativarLink);
+function galeriaClique(imagem) {
+  imagem.addEventListener("click", galeriaTrocar);
+}
+imagens.forEach(galeriaClique);
